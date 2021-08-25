2ef3dc570afa1f0920fccc4c57b8306244498feb
= <=>
2021-08-25 21:40:46 +0600

    fragments and navigation

---
+++ app/src/main/java/com/example/hwproject/fragments/FragmentActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hwproject.fragments
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.example.hwproject.R
+
+class FragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+
+    }
+
+
+}
