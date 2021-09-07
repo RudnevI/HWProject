191f6c8a667a6b5cb5f7757ce9fffc8b89e1773d
= <=>
2021-09-07 19:28:50 +0600

    testing token

--- app/src/main/java/com/example/hwproject/lesson_9/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/hwproject/lesson_9/ui/login/LoginViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.hwproject.lesson_9.ui.login
 
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.hwproject.R
 import com.example.hwproject.lesson_9.data.LoginRepository
+import com.example.hwproject.lesson_9.data.Result
 
-class LoginViewModel(private val loginRepository: LoginRepository): ViewModel() {
+class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
 
     private val _loginForm = MutableLiveData<LoginFormState>()
     val loginFormState: LiveData<LoginFormState> = _loginForm
@@ -18,7 +21,41 @@ class LoginViewModel(private val loginRepository: LoginRepository): ViewModel() 
 
         val result = loginRepository.login(username, password)
 
+        if (result is Result.Success) {
 
+            _loginResult.value =
+                LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+
+
+        } else {
+            _loginResult.value = LoginResult(error = R.string.login_failed)
+        }
+
+
+    }
+    fun loginDataChanged(username: String, password: String) {
+        if(!isUserNameValid(username)) {
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+        }
+        else if(!isPasswordValid(password)) {
+            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
+        }
+        else {
+            _loginForm.value = LoginFormState(isDataValid = true)
+        }
+    }
+
+    private fun isUserNameValid(username: String) : Boolean {
+        return if(username.contains('@')) {
+            Patterns.EMAIL_ADDRESS.matcher(username).matches()
+        }
+        else {
+            username.isNotBlank()
+        }
+    }
+
+    private fun isPasswordValid(password: String) : Boolean {
+        return password.length > 5
     }
 
 }
